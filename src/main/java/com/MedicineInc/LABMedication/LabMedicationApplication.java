a4a90dd329d3c5d4ea290b089969ccab1a528f92
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-09 16:53:06 -0300

    commit inicial

---
+++ src/main/java/com/MedicineInc/LABMedication/LabMedicationApplication.java
@@ -0,0 +1,13 @@
+package com.MedicineInc.LABMedication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabMedicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabMedicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MedicineInc/LABMedication/LabMedicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MedicineInc.LABMedication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabMedicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
