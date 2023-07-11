98f87268185e320475a11964b80011b3d585686f
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-11 16:02:58 -0300

    mudança no acesso dos atributos do EstadoCivilEnum

--- src/main/java/com/MedicineInc/LABMedication/enums/EstadoCivilEnum.java
+++ src/main/java/com/MedicineInc/LABMedication/enums/EstadoCivilEnum.java
@@ -1,15 +1,17 @@
 package com.MedicineInc.LABMedication.enums;
 
+import lombok.ToString;
+
 public enum EstadoCivilEnum {
     SOLTEIRO(1,"Solteiro"),
     CASADO(2,"Casado"),
     SEPARADO(3,"Separado"),
     DIVORCIADO(4,"Divorciado"),
     VIUVO(5,"Viúvo");
-    private int valor;
-    private String descricao;
+    private final int valor;
+    private final String descricao;
 
-    EstadoCivilEnum(int valor, String descricao) {
+     EstadoCivilEnum(int valor, String descricao){
         this.valor = valor;
         this.descricao = descricao;
     }
