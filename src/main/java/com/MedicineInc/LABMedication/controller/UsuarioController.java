d7da18a5c7bfbb1ab9e1e3e2fec5a3a518af86ad
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-13 13:13:09 -0300

    Adicionado exceções e rota para atualizar a senha do usuário

--- src/main/java/com/MedicineInc/LABMedication/controller/UsuarioController.java
+++ src/main/java/com/MedicineInc/LABMedication/controller/UsuarioController.java
@@ -1,8 +1,6 @@
 package com.MedicineInc.LABMedication.controller;
 
-import com.MedicineInc.LABMedication.dto.UsuarioAtualizacaoDto;
-import com.MedicineInc.LABMedication.dto.UsuarioCadastroDto;
-import com.MedicineInc.LABMedication.dto.UsuarioResponseDto;
+import com.MedicineInc.LABMedication.dto.*;
 import com.MedicineInc.LABMedication.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +8,8 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -24,6 +24,8 @@ public class UsuarioController {
             return new ResponseEntity<>(this.service.cadastrarUsuario(novoUsuario), HttpStatus.CREATED);
         } catch (DataIntegrityViolationException e) {
             return new ResponseEntity("Cpf já Cadastrado", HttpStatus.CONFLICT);
+        }catch (Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
 
 //        try {
@@ -43,9 +45,23 @@ public class UsuarioController {
     public ResponseEntity<UsuarioResponseDto> atualizarUsuario(@PathVariable Long identificador, @RequestBody @Valid UsuarioAtualizacaoDto usuarioAtualizado) {
         try {
             return new ResponseEntity(this.service.atualizarUsuario(identificador, usuarioAtualizado), HttpStatus.OK);
-        } catch (DataIntegrityViolationException e) {
+        } catch (NoSuchElementException e){
+            return new ResponseEntity("Usuário não encontrado", HttpStatus.NOT_FOUND);
+        }catch (DataIntegrityViolationException e) {
             return new ResponseEntity("Cpf já Cadastrado", HttpStatus.CONFLICT);
         } catch (Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping(value = "/{identificador}/senha")
+    public ResponseEntity<UsuarioResponseDto> atualizarSenhaUsuario(@PathVariable Long identificador, @RequestBody UsuarioAtualizacaoSenhaDto usuarioSenhaAtualizado) throws Exception {
+       try {
+           return new ResponseEntity(this.service.atualizarSenhaUsuario(identificador, usuarioSenhaAtualizado), HttpStatus.OK);
+       }catch (NoSuchElementException e){
+           return new ResponseEntity("Usuário não encontrado", HttpStatus.NOT_FOUND);
+       }
+       catch (Exception e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
