e1d44a967dcb20aa1032c95052374029159e452c
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-13 10:53:40 -0300

    criação dos dtos de retorno, atualização e cadastro a para se adequar as técnicas de mercado

---
+++ src/main/java/com/MedicineInc/LABMedication/dto/UsuarioCadastroDto.java
@@ -0,0 +1,60 @@
+package com.MedicineInc.LABMedication.dto;
+
+import com.MedicineInc.LABMedication.enums.EspecializacaoClinicaEnum;
+import com.MedicineInc.LABMedication.enums.EstadoCivilEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class UsuarioCadastroDto {
+
+    @NotBlank(message = "Nome Obrigatório")
+    private String nomeCompleto;
+
+    @NotBlank(message = "Gênero Obrigatório")
+    private String genero;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @NotNull(message = "Data inválida")
+    private LocalDate dataNascimento;
+
+    @NotNull(message = "CPF obrigatório")
+    @CPF(message = "Formato CPF inválido")
+    private String cpf;
+
+    @NotBlank(message = "Rg Obrigatório")
+    private String rg;
+
+    @NotNull(message = "Estado civil Obrigatório")
+    private EstadoCivilEnum estadoCivil;
+
+    @NotBlank(message = "Telefone Obrigatório")
+    private String telefone;
+
+    @Email(message = "Email inválido")
+    @NotBlank(message = "Email Obrigatório")
+    private String email;
+
+    @NotBlank(message = "Naturalidade Obrigatório")
+    private String naturalidade;
+
+    @NotBlank(message = "Crm Obrigatório")
+    private String crmUf;
+
+    @NotNull(message = "Especialização Obrigatório")
+    private EspecializacaoClinicaEnum especializacao;
+
+    @NotNull(message = "Campo senha obrigatório")
+    @Size(min = 8, message = "tamanho mínimo 8 caracteres")
+    @Size(max = 15, message = "tamanho maximo 15 caracteres")
+    private String senha;
+}
---
+++ src/main/java/com/MedicineInc/LABMedication/dto/UsuarioResponseDto.java
@@ -0,0 +1,66 @@
+package com.MedicineInc.LABMedication.dto;
+
+import com.MedicineInc.LABMedication.enums.EspecializacaoClinicaEnum;
+import com.MedicineInc.LABMedication.enums.EstadoCivilEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioResponseDto {
+
+    private Long id;
+
+    @NotBlank(message = "Nome Obrigatório")
+    private String nomeCompleto;
+
+
+    @NotBlank(message = "Gênero Obrigatório")
+    private String genero;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @NotNull(message = "Data invàlida")
+    private LocalDate dataNascimento;
+
+    @NotNull(message = "CPF obrigatório")
+    @CPF(message = "Formato CPF inválido")
+    private String cpf;
+
+    @NotBlank(message = "Rg Obrigatório")
+    private String rg;
+
+    @NotNull(message = "Estado civil Obrigatório")
+    private EstadoCivilEnum estadoCivil;
+
+    @NotBlank(message = "Telefone Obrigatório")
+    private String telefone;
+
+    @Email(message = "Email inválido")
+    @NotBlank(message = "Email Obrigatório")
+    private String email;
+
+    @NotBlank(message = "Naturalidade Obrigatório")
+    private String naturalidade;
+
+    @NotBlank(message = "Crm Obrigatório")
+    private String crmUf;
+
+    @NotNull(message = "Especialização Obrigatório")
+    private EspecializacaoClinicaEnum especializacao;
+    
+    @NotNull(message = "Campo senha obrigatório")
+    @Size(min = 8, message = "tamanho mínimo 8 caracteres")
+    @Size(max = 15, message = "tamanho maximo 15 caracteres")
+    private String senha;
+}
