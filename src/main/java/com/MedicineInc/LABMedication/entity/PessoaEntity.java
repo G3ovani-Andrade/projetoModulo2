40b8ac0f920b4bd0467df5b6fc164a41a64780d5
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-13 10:56:49 -0300

    Atualização da data, validação da data e retirada de notações sem uso

--- src/main/java/com/MedicineInc/LABMedication/entity/PessoaEntity.java
+++ src/main/java/com/MedicineInc/LABMedication/entity/PessoaEntity.java
@@ -4,16 +4,11 @@ package com.MedicineInc.LABMedication.entity;
 import com.MedicineInc.LABMedication.enums.EstadoCivilEnum;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-import lombok.*;
-import org.hibernate.validator.constraints.br.CPF;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.validation.annotation.Validated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Optional;
 
 @Getter @Setter
 @NoArgsConstructor
@@ -25,41 +20,31 @@ public abstract class PessoaEntity {
     private Long id;
 
     @Column(nullable = false)
-    @NotBlank(message = "Nome Obrigatório")
     private String nomeCompleto;
 
     @Column(nullable = false)
-    @NotBlank(message = "Gênero Obrigatório")
     private String genero;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
     @Column(nullable = false)
-    private Date dataNascimento;
+    private LocalDate dataNascimento;
 
     @Column(unique = true,nullable = false)
-    @NotNull(message = "CPF obrigatório")
-    @CPF(message = "Formato CPF inválido")
     private String cpf;
 
     @Column(nullable = false)
-    @NotBlank(message = "Rg Obrigatório")
     private String rg;
 
     @Column(nullable = false)
-    @NotNull(message = "Estado civil Obrigatório")
     private EstadoCivilEnum estadoCivil;
 
     @Column(nullable = false)
-    @NotBlank(message = "Telefone Obrigatório")
     private String telefone;
 
     @Column(nullable = false)
-    @Email(message = "Email inválido")
-    @NotBlank(message = "Email Obrigatório")
     private String email;
 
     @Column(nullable = false)
-    @NotBlank(message = "Naturalidade Obrigatório")
     private String naturalidade;
 
 }
