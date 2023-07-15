793560b84e063c8abbe5c468eff425fc278b38af
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-15 14:42:56 -0300

    adicionado os construtores vazio e com todos os atributos

--- src/main/java/com/MedicineInc/LABMedication/entity/EnderecoEntity.java
+++ src/main/java/com/MedicineInc/LABMedication/entity/EnderecoEntity.java
@@ -1,12 +1,17 @@
 package com.MedicineInc.LABMedication.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 @Entity
 @Table(name = "endereco")
 @Getter @Setter
-public class EnderecoEntity {
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnderecoEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
