b90a93aeea729c5fd7058c18c101f355895a6c7f
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-19 11:53:30 -0300

    adição de @Repository que faltava no repository de administração de medicamento

--- src/main/java/com/MedicineInc/LABMedication/repository/MedicamentoRepository.java
+++ src/main/java/com/MedicineInc/LABMedication/repository/MedicamentoRepository.java
@@ -2,9 +2,11 @@ package com.MedicineInc.LABMedication.repository;
 
 import com.MedicineInc.LABMedication.entity.MedicamentoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MedicamentoRepository extends JpaRepository<MedicamentoEntity,Long> {
 
     List<MedicamentoEntity> findByPacienteId(Long identificador);
