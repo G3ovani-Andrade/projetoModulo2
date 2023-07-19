84e2bf40bbbe1a27a9da405f592d7ff0983f1cb7
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-19 15:08:06 -0300

    criação da rota para atualizar a administração de medicamento

--- src/main/java/com/MedicineInc/LABMedication/controller/AdministracaoMedicamentoController.java
+++ src/main/java/com/MedicineInc/LABMedication/controller/AdministracaoMedicamentoController.java
@@ -1,5 +1,6 @@
 package com.MedicineInc.LABMedication.controller;
 
+import com.MedicineInc.LABMedication.dto.AdministracaoMedicamentoAtualizacaoDto;
 import com.MedicineInc.LABMedication.dto.AdministracaoMedicamentoCadastroDTO;
 import com.MedicineInc.LABMedication.dto.AdministracaoMedicamentoResponseDTO;
 import com.MedicineInc.LABMedication.service.AdministracaoMedicamentoService;
@@ -8,11 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicamentos")
@@ -29,12 +26,19 @@ public class AdministracaoMedicamentoController {
         }catch (Exception e){
             return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+    }
 
-
-
-
-
+    @PutMapping("/{identificador}")
+    public ResponseEntity<AdministracaoMedicamentoResponseDTO> atualizarAdimistracaoMedicamento(@PathVariable Long identificador,@RequestBody @Valid AdministracaoMedicamentoAtualizacaoDto medicamentoAtualizado){
+        try {
+            return new ResponseEntity<>(this.service.atualizarAdimistracaoMedicamento(identificador,medicamentoAtualizado), HttpStatus.OK);
+        }catch (EntityNotFoundException e){
+            return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e){
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
 
     }
+
 
 }
