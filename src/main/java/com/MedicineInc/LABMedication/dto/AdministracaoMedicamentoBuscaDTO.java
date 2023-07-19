608f0dd2cb941f8035c643e36863e0246e94c445
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-19 17:12:52 -0300

    criação de dtos para atualização e resposta de administração de medicamento

--- src/main/java/com/MedicineInc/LABMedication/dto/AdministracaoMedicamentoBuscaDTO.java
+++ src/main/java/com/MedicineInc/LABMedication/dto/AdministracaoMedicamentoBuscaDTO.java
@@ -3,8 +3,6 @@ package com.MedicineInc.LABMedication.dto;
 import com.MedicineInc.LABMedication.enums.TipoMedicamentoEnum;
 import com.MedicineInc.LABMedication.enums.UnidadeEnum;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,33 +13,28 @@ import java.time.LocalDateTime;
 @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class AdministracaoMedicamentoResponseDTO {
-
+public class AdministracaoMedicamentoBuscaDTO {
     private Long id;
 
-    @NotBlank(message = "Nome do medicamento obrigatório")
     private String nomeMedicamento;
 
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
-    @NotNull(message = "Data inválida")
     private LocalDateTime administracao;
 
     private TipoMedicamentoEnum tipo;
 
-    @NotBlank(message = "Quantidade obrigatório")
     private int quantidade;
 
     private UnidadeEnum unidade;
 
-    @NotBlank(message = "Observações obrigatório")
     private String observacoes;
 
     //mappeamento para medico
-    @NotNull(message = "Usuário obrigatório")
-    private Long identificador_usuario;
+
+    private UsuarioResponseDto usuario;
     // mappeamento para paciente
-    @NotNull(message = "Paciente obrigatório")
-    private Long identificador_paciente;
+
+    private PacienteResponseDto paciente;
 
     public String getTipo() {
         return tipo.getDescricao();
