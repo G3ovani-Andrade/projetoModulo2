80b829740dab6a2f7dfe7005fcb1010b9df9fe9c
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-20 19:30:21 -0300

    rota para buscar estatísticas

--- src/main/java/com/MedicineInc/LABMedication/repository/MedicamentoRepository.java
+++ src/main/java/com/MedicineInc/LABMedication/repository/MedicamentoRepository.java
@@ -1,7 +1,9 @@
 package com.MedicineInc.LABMedication.repository;
 
+import com.MedicineInc.LABMedication.dto.EstatisticaDTO;
 import com.MedicineInc.LABMedication.entity.MedicamentoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,7 @@ import java.util.List;
 public interface MedicamentoRepository extends JpaRepository<MedicamentoEntity,Long> {
 
     List<MedicamentoEntity> findByPacienteId(Long identificador);
+
+    @Query(value = "SELECT P.NOME_COMPLETO as nome_completo,COUNT(*)as total_medicamentos FROM MEDICAMENTO M JOIN PACIENTE P ON P.ID = M.PACIENTE_ID GROUP BY P.ID",nativeQuery = true)
+    List<EstatisticaDTO> estatisticas();
 }
