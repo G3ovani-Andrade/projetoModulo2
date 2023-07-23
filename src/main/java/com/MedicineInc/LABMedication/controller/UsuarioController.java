22671333c498a827601f1998a7f9cb102c1e38e1
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-23 15:36:01 -0300

    Correção dos tratamentos para um tratamento global

--- src/main/java/com/MedicineInc/LABMedication/controller/UsuarioController.java
+++ src/main/java/com/MedicineInc/LABMedication/controller/UsuarioController.java
@@ -1,15 +1,15 @@
 package com.MedicineInc.LABMedication.controller;
 
-import com.MedicineInc.LABMedication.dto.*;
+import com.MedicineInc.LABMedication.dto.UsuarioAtualizacaoDTO;
+import com.MedicineInc.LABMedication.dto.UsuarioAtualizacaoSenhaDTO;
+import com.MedicineInc.LABMedication.dto.UsuarioCadastroDTO;
+import com.MedicineInc.LABMedication.dto.UsuarioResponseDTO;
 import com.MedicineInc.LABMedication.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -19,50 +19,17 @@ public class UsuarioController {
     private UsuarioService service;
 
     @PostMapping
-    public ResponseEntity<UsuarioResponseDto> cadastrarUsuarios(@RequestBody @Valid UsuarioCadastroDto novoUsuario) {
-        try {
+    public ResponseEntity<UsuarioResponseDTO> cadastrarUsuarios(@RequestBody @Valid UsuarioCadastroDTO novoUsuario) {
             return new ResponseEntity<>(this.service.cadastrarUsuario(novoUsuario), HttpStatus.CREATED);
-        } catch (DataIntegrityViolationException e) {
-            return new ResponseEntity("Cpf já Cadastrado", HttpStatus.CONFLICT);
-        }catch (Exception e) {
-            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-
-//        try {
-//            return new ResponseEntity(this.service.cadastrarUsuario(novoUsuario),HttpStatus.CREATED);
-//        }catch (DataIntegrityViolationException e){
-//            return new ResponseEntity("Cpf já Cadastrado",HttpStatus.CONFLICT);
-//       } catch (ConstraintViolationException e){
-//            String erro = "";
-//            for (ConstraintViolation<?> c : e.getConstraintViolations()){
-//                erro += c.getMessage() + c.getPropertyPath()+"\n";
-//            }
-//            return new ResponseEntity(erro,HttpStatus.BAD_REQUEST);
-//        }
     }
 
     @PutMapping(value = "/{identificador}")
-    public ResponseEntity<UsuarioResponseDto> atualizarUsuario(@PathVariable Long identificador, @RequestBody @Valid UsuarioAtualizacaoDto usuarioAtualizado) {
-        try {
-            return new ResponseEntity(this.service.atualizarUsuario(identificador, usuarioAtualizado), HttpStatus.OK);
-        } catch (NoSuchElementException e){
-            return new ResponseEntity("Usuário não encontrado", HttpStatus.NOT_FOUND);
-        }catch (DataIntegrityViolationException e) {
-            return new ResponseEntity("Cpf já Cadastrado", HttpStatus.CONFLICT);
-        } catch (Exception e) {
-            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<UsuarioResponseDTO> atualizarUsuario(@PathVariable Long identificador, @RequestBody @Valid UsuarioAtualizacaoDTO usuarioAtualizado) {
+            return new ResponseEntity<>(this.service.atualizarUsuario(identificador, usuarioAtualizado), HttpStatus.OK);
     }
 
     @PatchMapping(value = "/{identificador}/senha")
-    public ResponseEntity<UsuarioResponseDto> atualizarSenhaUsuario(@PathVariable Long identificador, @RequestBody UsuarioAtualizacaoSenhaDto usuarioSenhaAtualizado) throws Exception {
-       try {
-           return new ResponseEntity(this.service.atualizarSenhaUsuario(identificador, usuarioSenhaAtualizado), HttpStatus.OK);
-       }catch (NoSuchElementException e){
-           return new ResponseEntity("Usuário não encontrado", HttpStatus.NOT_FOUND);
-       }
-       catch (Exception e) {
-            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<UsuarioResponseDTO> atualizarSenhaUsuario(@PathVariable Long identificador, @RequestBody @Valid UsuarioAtualizacaoSenhaDTO usuarioSenhaAtualizado) throws Exception {
+           return new ResponseEntity<>(this.service.atualizarSenhaUsuario(identificador, usuarioSenhaAtualizado), HttpStatus.OK);
     }
 }
