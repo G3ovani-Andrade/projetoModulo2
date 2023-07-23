3d6739072f95ecfcc3c528d6cfb1935f23319e69
G3ovani-Andrade <124536228+G3ovani-Andrade@users.noreply.github.com>
2023-07-23 15:37:52 -0300

    criação do DTO para validação e classe para validação

---
+++ src/main/java/com/MedicineInc/LABMedication/exceptions/TratadorDeExcessoes.java
@@ -0,0 +1,43 @@
+package com.MedicineInc.LABMedication.exceptions;
+
+import com.MedicineInc.LABMedication.dto.validacao.ErroValidacaoDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class TratadorDeExcessoes {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> tratarEntityNotFound(EntityNotFoundException e){
+        return new ResponseEntity(e.getMessage(),HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity tratarExcessaoEnum(IllegalArgumentException e){
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErroValidacaoDTO>> tratarParametroInvalido(MethodArgumentNotValidException e){
+        List<FieldError> errors = e.getFieldErrors();
+        List<ErroValidacaoDTO> errosDTO = new ArrayList<>();
+        for(FieldError erro : errors){
+            errosDTO.add(new ErroValidacaoDTO(erro));
+        }
+        return new ResponseEntity<>(errosDTO,HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> tratarConflitoCpf(DataIntegrityViolationException e){
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+    }
+
+}
